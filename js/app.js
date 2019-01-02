b0c0b3641a61474911fee29dd1df6da66fa5c3e1
Kevin <k.cuison@gmail.com>
2019-01-02 11:23:07 -0800

    Create Boilerplate

---
+++ js/app.js
@@ -0,0 +1,12 @@
+console.log("ready");
+
+/*----- app's state (variables) -----*/
+
+
+/*----- cached element references -----*/ 
+
+
+/*----- event listeners -----*/ 
+
+
+/*----- functions -----*/
